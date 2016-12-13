d75922296cf863c3d8aa6e44e153f12f74f5f34c
PurifyPioneer <klook.christian@gmail.com>
2016-12-13 11:51:23 +0100

    Progress from Series 7.

--- Übungen/src/programming/set7/sudoku/Sudoku.java
+++ Übungen/src/programming/set7/sudoku/Sudoku.java
@@ -8,77 +8,102 @@ public class Sudoku extends NumberBoard {
 
 	@Override
 	public void setValueAt(int row, int col, int value) {
-		if(value == EMPTY || (value >= 1 && value <= 9)) {
-			super.setValueAt(row, col, value);
+		if (value == EMPTY || (value >= 1 && value <= 9)) {
+			super.setValueAt(row - 1, col - 1, value);
 		}
 	}
-	
+
 	public boolean isValid() {
-		
-		//Check rows and columns
-		int[] numberCount_rows = new int[9];
-		int[] numberCount_columns = new int[9];
-		
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if(!(getValueAt(j, i) == EMPTY)) {
-					numberCount_rows[getValueAt(j, i)-1]++;
-				}
-				if(!(getValueAt(i, j) == EMPTY)) {
-					numberCount_columns[getValueAt(i, j)-1]++;
-				}
-			}
-		}
-		
-		for (int i = 0; i < 9; i++) {
-			if((numberCount_rows[i] > 1) || (numberCount_columns[i] > 1)) {
+
+		if (isValid(1)) {
+			if (!isValid(2)) {
 				return false;
 			}
+		} else {
+			return false;
 		}
-		
-		//Check 3x3 squares
-		int[] numberCount_InSquare = new int[9];
-		
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				for (int i1 = 0; i1 < 3; i1++) {
-					for (int j1 = 0; j1 < 3; j1++) {
-						if(!(getValueAt(i1 + (i* 3), j1 + (j* 3)) == EMPTY)) {
-							numberCount_InSquare[getValueAt(i1 + (i* 3), j1 + (j* 3))-1]++;
-						}
+
+		return true;
+	}
+
+	//TODO Look at selection method.. Anton
+	private boolean isValid(int mode) {
+
+		if (mode == 1) {
+			// Check rows and columns
+			int[] numberCount_rows = new int[9];
+			int[] numberCount_columns = new int[9];
+
+			// Check rows ---> i repesents row-number and
+			// j represents each element of the row.
+			// If the value at the desired position is not empty the value
+			// will be stored in the array numberCount-rows.
+			// TODO adjust comments
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					if (getValueAt(i, j) != EMPTY) {
+						numberCount_rows[getValueAt(i, j) - 1]++;
+					}
+					if (!(getValueAt(j, i) == EMPTY)) {
+						numberCount_columns[getValueAt(j, i) - 1]++;
 					}
 				}
-				
-				for (int i2 = 0; i < 9; i++) {
-					if(numberCount_InSquare[i2] > 1) {
+				for (int j = 0; j < numberCount_rows.length; j++) {
+					if (numberCount_rows[j] > 1) {
+						return false;
+					}
+					if (numberCount_columns[j] > 1) {
 						return false;
 					}
 				}
+				numberCount_rows = new int[9];
+				numberCount_columns = new int[9];
 			}
 		}
-		
-		
+		if (mode == 2) {
+			
+			int[] numberCount_small = new int[9];
+			
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					
+					for (int j2 = 0; j2 < 3; j2++) {
+						for (int k = 0; k < 3; k++) {
+							if (!(getValueAt(j2 + (i*3), k + (j*3)) == EMPTY)) {
+								numberCount_small[getValueAt(j2 + (i*3), k + (j*3)) - 1]++;
+							}
+						}
+					}
+					for (int j2 = 0; j2 < numberCount_small.length; j2++) {
+						if(numberCount_small[j2] > 1) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+
 		return true;
 	}
-	
+
 	@Override
 	public String toString() {
 
 		String[] lines = new String[9];
-		
+
 		for (int i = 0; i < 9; i++) {
 			lines[i] = "";
 			for (int j = 0; j < 9; j++) {
-				lines[i] += Integer.toString(getValueAt(i, j));
+				lines[i] += Integer.toString(getValueAt(i, j)) + " ";
 			}
 		}
-		
+
 		String finalString = "";
-	
+
 		for (int i = 0; i < lines.length; i++) {
 			finalString += lines[i] + "\n";
 		}
-		
+
 		return finalString;
 	}
 }
--- Übungen/src/programming/set7/sudoku/SudokuTest.java
+++ Übungen/src/programming/set7/sudoku/SudokuTest.java
@@ -4,10 +4,14 @@ public class SudokuTest {
 
 	public static void main(String[] args) {
 		
-		NumberBoard sudoku = new Sudoku();
+		Sudoku sudoku = new Sudoku();
+		
+		sudoku.setValueAt(1, 1, 5);
+		sudoku.setValueAt(3, 3, 5);
+		sudoku.setValueAt(2, 4, 5);
 		
 		System.out.println(sudoku);
 		
+		System.out.println(sudoku.isValid());	
 	}
-	
 }
